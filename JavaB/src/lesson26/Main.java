3ddcc5bf095574d9a5d16ecab1f4ac7172a4bc2b
ssultannn <smammadkhanov@gmail.com>
2025-02-03 22:46:47 +0400

    l25 class Object

---
+++ JavaB/src/lesson26/Main.java
@@ -0,0 +1,10 @@
+package lesson26;
+
+public class Main {
+	public static void main(String[] args) {
+		Person person = new Person("ali", 12);
+		Person person2 = new Person("al", 12);
+		System.out.println(person.equals(person2));
+
+	}
+}
---
+++ JavaB/src/lesson26/Person.java
@@ -0,0 +1,35 @@
+package lesson26;
+
+import java.util.Objects;
+
+public class Person {
+	String name;
+	int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return age == other.age && Objects.equals(name, other.name);
+	}
+
+
+
+	
+
+}
